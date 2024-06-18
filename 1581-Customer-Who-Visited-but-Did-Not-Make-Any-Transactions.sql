9c8ac0769bebd4c25c19db5f9743e539b7cd86e1
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-06-18 08:17:35 +0300

    [1581] [Time Beats: undefined] - LeetPush

---
+++ 1581-Customer-Who-Visited-but-Did-Not-Make-Any-Transactions.sql
@@ -0,0 +1,6 @@
+SELECT V.customer_id , COUNT(V.customer_id ) AS count_no_trans
+FROM Visits V
+LEFT JOIN Transactions T ON V.visit_id = T.visit_id
+WHERE T.transaction_id IS NULL
+GROUP BY V.customer_id ;
+-- squence left join - where - groub by - aggregate
