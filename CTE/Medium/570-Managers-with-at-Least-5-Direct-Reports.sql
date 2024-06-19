cb35c83af0e992167b66a83c8b51fa60f762b08f
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-06-19 07:22:46 +0300

    Update 570-Managers-with-at-Least-5-Direct-Reports.sql

--- CTE/Medium/570-Managers-with-at-Least-5-Direct-Reports.sql
+++ CTE/Medium/570-Managers-with-at-Least-5-Direct-Reports.sql
@@ -1,3 +1,4 @@
+-- problem Link = https://leetcode.com/problems/managers-with-at-least-5-direct-reports/?envType=study-plan-v2&envId=top-sql-50
 WITH ManagerEmployeeCounts AS (
     SELECT 
         E1.name,
